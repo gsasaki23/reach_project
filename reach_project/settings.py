e0c64417eb1d1fc5f475e56ab48ed9e0cc5c5fc3
Gaku Sasaki <gaku.sasaki@gmail.com>
2020-02-28 16:13:58 -0800

    animations

--- reach_project/settings.py
+++ reach_project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h79(y1@apm*0k*8_xgyp0#hqm%0m2mwmiu+$#4qici#d^&zm(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+# C H A N G E   T O    []   I F    L O C A L !!!!!!!!!
+ALLOWED_HOSTS = ['18.188.185.147']
 
 
 # Application definition
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# C O M M E N T    I F    L O C A L!!!
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
